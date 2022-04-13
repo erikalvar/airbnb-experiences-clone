f83783e47cdb018cf383bedf1ff27d9206617d49
Erik Alvar <erikalvarbass@gmail.com>
2022-04-13 10:34:06 -0400

    adds more styling

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,19 +6,19 @@ export default function Card() {
   return (
       <div className="card">
         <div className="card--photo">
-          <h3>Online</h3>
+          
           <img src={katie} alt="" />
         </div>
         <div className="card--data">
           <img src={Star} alt="" />
-          <p>5.0</p>
-          <p>(6)·USA</p>
+          <p className="gray">5.0</p>
+          <p className="gray">(6)·USA</p>
         </div>
         <div className="card--description">
           <p>Life lessons with Katie Zaferes</p>
         </div>
         <div className="card--price">
-          <p className="card--price--rate">From $136 /</p>
+          <p className="card--price--rate bold">From $136 /</p>
           <p className="card--price--person">person</p>
         </div>
       </div>
